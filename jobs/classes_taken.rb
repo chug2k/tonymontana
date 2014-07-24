22d09f90bb7ddb5549c75148617cbc7962ff232c
Charles Lee <chug2k@gmail.com>
2014-07-23 23:23:15 -0700

    woo hoo hoo hoo hoo

--- jobs/classes_taken.rb
+++ jobs/classes_taken.rb
@@ -1,5 +1,5 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '10000s', :first_in => 0 do |job|
+SCHEDULER.every '5s', :first_in => 0 do |job|
   num_classes = 30
   num_classes_last = 10
   cost_per_class = 20.0
@@ -14,18 +14,39 @@ SCHEDULER.every '10000s', :first_in => 0 do |job|
   send_event('calories_burned', { current: num_classes * calories_per_class, last: num_classes_last * calories_per_class})
 
 
-  series = [
-      {
-          name: 'Classes',
-          data: [
-              {x:0, y: 4}, {x:1, y:9}, {x:2, y:6}, {x:3, y: 3},
-              {x:4, y: 1}, {x:5, y:2}, {x:6, y:7}, {x:7, y: 3},
-              {x:8, y: 4}, {x:9, y:2}
-          ]
-      }
+  # urgh
+  data = {
+    labels: [
+      'Mo 5:30p - Pac Heights',
+      'Tu 6:00a - Dolores',
+      'Tu 7:00p - Dolores',
+      'Tu 9:00a - Dolores',
+      'Tu 6:30p - Embarcadero',
 
-  ]
+      'We 5:30p - Pac Heights',
+      'Th 6:00a - Dolores',
+      'Th 7:00p - Dolores',
+      'Th 9:00a - Dolores',
+      'Th 6:30p - Embarcadero'
 
-  send_event('attendance_by_class', series: series, xAxisLabels: {});
+
+
+    ],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [5, 4, 9, 15, 12, 13, 5, 2, 10, 2]
+        }
+    ]
+  }
+
+  send_event('attendance_by_class', data);
+
 
 end
--- widgets/chartjs/chartjs.coffee
+++ widgets/chartjs/chartjs.coffee
@@ -1,14 +1,30 @@
 class Dashing.Chartjs extends Dashing.Widget
 
-@accessor 'current', Dashing.AnimatedValue
+  ready: () ->
+    $canvas = $('canvas', $(@node))
 
-ready: ->
-  @chart = new Highcharts.Chart(
-    chart:
-      renderTo: @node
-  )
+    # Wow, this is shitty.
+    $container = $(@node).parent()
+    if $container.data('sizex') == 3 && $container.data('sizey') == 1
+      $canvas.attr 'width', 900
+      $canvas.attr 'height', 250
 
-onData: (data) ->
-  @chart = new Highcharts.Chart(
-    @get('data')
-  )
+    if @get('labels') && @get('datasets')
+      @.renderGraph
+        labels: @get('labels')
+        datasets: @get('datasets')
+
+
+  onData: (data) ->
+    @.renderGraph(data)
+
+
+  renderGraph: (data) ->
+    $canvas = $('canvas', $(@node))
+    if $canvas.get(0)
+      ctx = $canvas.get(0).getContext('2d')
+      ctx.clearRect(0, 0, $canvas.width, $canvas.height)
+      chart = new Chart(ctx).Bar(data, showLabels: false)
+
+
+
